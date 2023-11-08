fa9d657bf17697a1ada835e2d9353d49628cf4f6
S-a-b-b-i-R <sabbirssmc90@gmail.com>
2023-11-08 14:29:36 +0600

    minor change

--- src/Pages/UpdateJob.jsx
+++ src/Pages/UpdateJob.jsx
@@ -62,7 +62,6 @@ const UpdateJob = () => {
             salaryrange,
             lastDate,
         };
-        console.log(jobData);
 
         try {
             await fetch(`http://localhost:5000/api/jobs/${id}`, {
